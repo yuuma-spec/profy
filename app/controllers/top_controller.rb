d1389d1cd5886d9fda6c70bb4121c0b142a5e22a
EC2 Default User <ec2-user@ip-172-31-21-244.ap-northeast-1.compute.internal>
2021-06-15 05:23:21 +0000

    profy commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,8 @@
+class TopController < ApplicationController
+
+  def index
+    @question = Question.new
+    @feed_contents = current_user.group.feed_contents.includes(:content).page(params[:page]).per(5)
+    @feed_contents_resource = @feed_contents.map(&:content)
+  end
+end
